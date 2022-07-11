c8345d951618d37f075cd5955e63188e89620065
joonhyunpark <channelsucre@gmail.com>
2022-07-11 13:18:50 +0900

    UI minor changes

--- front_end/MadCamp/app/src/main/java/com/chajun/madcamp/config/Constant.java
+++ front_end/MadCamp/app/src/main/java/com/chajun/madcamp/config/Constant.java
@@ -2,8 +2,8 @@ package com.chajun.madcamp.config;
 
 public class Constant {
     public final static String kakaoNativeKey = "b8184b5b4a5d3896f2c1e1196805c056";
-    public static final String BASE_URL = "http://172.10.18.163:443"; //NAT IP
-//    public static final String BASE_URL = "http://192.249.18.163:443"; //공인 IP
+//    public static final String BASE_URL = "http://172.10.18.163:443"; //NAT IP
+    public static final String BASE_URL = "http://192.249.18.163:443"; //공인 IP
 
     public final static int BUILD_DECK_MAX_COUNT_DOWN = 5;
     public final static int TURN_MAX_COUNT_DOWN = 3;
